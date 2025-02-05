12c367dedf06ce2e5b29620b724c9791c6df988c
SpencerWF <spencer@machnet-robotics.com>
2025-02-05 17:19:28 +0000

    Adding comment

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
 require("s.lazy")
 require("s.settings")
+require("nvim-tree").setup()
+require("Comment").setup()
 vim.opt.clipboard = "unnamedplus"
